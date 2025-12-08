106bdb2b04dfbf6896c5a9e66a4baf3a72c32042
JaimeCanales <ja.canales@duocuc.cl>
2025-12-08 14:09:30 -0300

    Reparación a algunas rutas rotas

--- Capstone/Proyecto/sistema/sistema/urls.py
+++ Capstone/Proyecto/sistema/sistema/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,3 +12,7 @@ urlpatterns = [
     path("cuentas/", include(("cuentas.urls", "cuentas"), namespace="cuentas")),
     path("accounts/", include("django.contrib.auth.urls")),  # ← login/logout/reset
 ]
+
+# Servir archivos de media en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
